b961a89d4fb38a894deb2949b85cb7ec412e5f5d
ayan4m1 <andrew@bulletlogic.com>
2022-06-17 15:15:44 -0400

    clean up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,31 +1,12 @@
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-
 import Layout from '~components/Layout';
 import SEO from '~components/SEO';
 
-export default function NotFoundPage({ data }) {
-  const siteTitle = data.site.siteMetadata.title;
-
+export default function NotFoundPage() {
   return (
-    <Layout title={siteTitle}>
-      <SEO title="404: Not Found" />
+    <Layout>
+      <SEO title="Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>
   );
 }
-
-NotFoundPage.propTypes = {
-  data: PropTypes.object.isRequired
-};
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
